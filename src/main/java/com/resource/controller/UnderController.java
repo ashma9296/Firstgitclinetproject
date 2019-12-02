c84c841e144d0057f79d9bdff221a726bb275940
ashma9296 <ashma.khadka2019@gmail.com>
2019-12-02 18:07:37 -0500

    CRP-505 newly modified

--- src/main/java/com/resource/controller/UnderController.java
+++ src/main/java/com/resource/controller/UnderController.java
@@ -3,17 +3,17 @@ package com.resource.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 @Controller
 
-public class Rentcontroller {
-	
-	@RequestMapping(value= "inside", method= RequestMethod.GET )
+public class UnderController {
+
+	@RequestMapping(value= "upside", method= RequestMethod.GET )
 	public String terming() {
 		System.out.println(" it will print");
 		System.out.println(" TIME DEMAND ");
-		return "insidestory";
+		return "therefore";
 	}
 	
+
 
 }
